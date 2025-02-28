274023689c6f3faad22e3b47c7975999b673cf32
pavani-bandaru <149463515+pavani-bandaru@users.noreply.github.com>
2025-02-28 13:53:18 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import google.generativeai as genai
+API_KEY = "AIzaSyA2XYTLxP5QV1BpvYbJE1OE9aD3cyiihc0"
+genai.configure(api_key=API_KEY)
+model = geneai.GenerativeModel('gemini-1.5-flash')
+
+if "chat" not in st.session_state:
+  st.session_state.chat = model.start_chat(history=[])
+
+st.title("Chatbot- Your AI assistant")
+if "messages" not in st.session_state:
+  st.session_state.messages = []
+for message in st.session_state.messages:
+  with st.chat_message(message["role"]):
+    st.markdown(message["content"])
+
+if prompt := st.chat_input("Say something..."):
+  st.session_state.messages.append({"role":"user", "content":prompt})
+  with st.chat_message("user"):
+    st.markdown(prompt)
+  response = st.session_state.chat.send_message(prompt)
+  st.session_state.messages.append({"role":"assistant", "content":response.text})
